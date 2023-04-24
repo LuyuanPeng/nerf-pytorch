fe44fca8448d867935d2088ee2aa0826986986b4
luyuan-super <luyuan@nus.edu.sg>
2023-04-24 16:03:12 +0800

    udpates

--- run_nerf.py
+++ run_nerf.py
@@ -18,8 +18,8 @@ from load_deepvoxels import load_dv_data
 from load_blender import load_blender_data
 from load_LINEMOD import load_LINEMOD_data
 
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 np.random.seed(0)
 DEBUG = False
 
